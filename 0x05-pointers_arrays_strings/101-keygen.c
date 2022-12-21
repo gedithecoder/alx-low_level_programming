f405b15268180ad1cc9cf844aba445b6a4f9b753
gedithecoder <gedionderejev@gmail.com>
2022-12-21 06:08:11 -0800

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - generates random valid passwords
+ * Return: 0 (on sucess)
+ *
+ */
+int main(void)
+{
+	int pass, sum;
+
+	strand(time(NULL));
+	sum = 0
+		while (sum <= 2645)
+		{
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
+		}
+		printf("%c", 2772 - sum);
+
+		return (0);
+}
