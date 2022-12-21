f35f8722edaab05e52b09f521faea80c619139f2
gedithecoder <gedionderejev@gmail.com>
2022-12-21 11:51:28 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ */
+void print_alphabet(void)
+{
+	char c = "a";
+
+	while (c <= "z")
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
