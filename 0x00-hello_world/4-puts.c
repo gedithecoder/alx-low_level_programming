89dee37114ad1ab0024a810379b42cb9f4d6fc61
gedithecoder <gedionderejev@gmail.com>
2022-12-21 08:17:19 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include "main.h"
 
-/*
- * main - print a given phrase
+/**
+ * main - Entry point
+ * Description - print a given phrase using puts
  *
- * Return - Always 0 (success)
+ * Return: 0
  */
 
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 {
-} return 0;
+} return (0);
 }
