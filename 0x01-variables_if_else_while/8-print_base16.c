80771e03650b02d6d2f5cdf4b617f2dea5d4b480
gedithecoder <gedionderejev@gmail.com>
2022-12-21 17:13:23 -0800

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,20 +11,18 @@ int main(void)
 	int num;
 
 
-		for (num = 0; num < 16; num++);
-		{
+	for (num = 0; num < 16; num++);
+	{
 		if (num < 10)
 		{
 			putchar(num + '0');
-		}
-		else
-
+		} else
 		{
 
 			putchar(c)
 			c++;
 		}
-		}
-		putchar('\n');
-		return (0);
+	}
+	putchar('\n');
+	return (0);
 }
