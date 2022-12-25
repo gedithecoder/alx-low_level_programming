03320a3eade75d7b27ea286b2e85e0daf4798935
gedithecoder <gedionderejev@gmail.com>
2022-12-25 13:45:10 -0800

    Task 4

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 times the number, from 0 to 14
+ * _putchar only 3 times
+ * Return 0-14 10 10times
+ */
+void more_numbers(void)
+{
+	int i, ro;
