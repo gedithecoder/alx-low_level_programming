7c220dba0ca49add85effb34667eff3d346ad45b
gedithecoder <gedionderejev@gmail.com>
2023-01-01 03:17:54 -0800

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,21 +8,19 @@
 int main(void)
 {
 	char c;
-	int num;
+	int d;
 
-
-	for (num = 0; num < 16; num++);
-	{
-		if (num < 10)
-		
-			putchar(num + '0');
-		else
-		{
-
+	c = 'a'
+	while
+		d < 10 {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
 			putchar(c);
 			c++;
 		}
-	}
 	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+	putchar(number + '0');
+
+	if (number < 9)
+	{
+		putchar('_');
+		putchar('
