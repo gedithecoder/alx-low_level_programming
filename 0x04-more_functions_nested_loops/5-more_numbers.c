f5cd9c4f42fdac7fc90aac6490d106e4be8cd120
gedithecoder <gedionderejev@gmail.com>
2023-01-03 14:14:29 -0800

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,8 +3,22 @@
 /**
  * more_numbers - function that prints 10 times the number, from 0 to 14
  * _putchar only 3 times
- * Return 0-14 10 10times
+ *
+ * Return: 0-14 10 times
  */
 void more_numbers(void)
 {
-	int i, ro;
+int number1, number2;
+
+for (number1 = 0; number1 < 10; number1++)
+{
+for (number2 = 0; number <= 14; number2++)
+{
+if (number2 < 9)
+{
+_putchar((number2 / 10) + '0');
+_putchar((number2 % 10) + '0');
+{
+_putchar(10);
+}
+}
