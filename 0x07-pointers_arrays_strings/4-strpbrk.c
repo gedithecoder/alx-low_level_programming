7ba71ea66ea2cf82840c6af252562374cbc449f1
gedithecoder <gedionderejev@gmail.com>
2023-01-03 18:01:59 -0800

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,16 +10,15 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int K;
+	int k;
 
 	while (*s)
 	{
 		for (k = 0; accept[k]; k++)
 		{
-			if (*s == accept[k])
-			{
-			return (s);
-			}
+		if (*s == accept[k])
+		return (s);
+		}
 	s++;
 	}
 	return ('\0');
