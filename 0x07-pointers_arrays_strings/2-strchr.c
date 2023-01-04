4a18179591a6411e74ce6a893b97727272f184bf
gedithecoder <gedionderejev@gmail.com>
2023-01-03 17:22:43 -0800

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,17 +9,12 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		++s;
-	}
-	if (*s == c)
-	{
-		return (s);
+		if (s[i] == c)
+			return (&s[i]);
 	}
 	return (0);
 }
