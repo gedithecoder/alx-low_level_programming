0daf155aca87d33165cbe3c4d58000e69f07e6b7
gedithecoder <gedionderejev@gmail.com>
2023-01-05 15:59:19 -0800

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print the length of the string
+ * @s: the sring to be measured
+ *
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+}
