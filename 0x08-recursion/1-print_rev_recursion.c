a2e9cec4d20146317f05526256d1c5c036844481
gedithecoder <gedionderejev@gmail.com>
2023-01-05 15:42:54 -0800

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_rev_recursion - prints a string in reverse
+ * @s: The string to be printed
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+
+}
