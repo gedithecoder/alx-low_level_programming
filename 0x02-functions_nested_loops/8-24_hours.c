e469ad2c3243755ab99c0e5c57e0fd4bd25a1ffa
gedithecoder <gedionderejev@gmail.com>
2023-01-16 09:05:02 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jacj_bauer - print hours upto 23:59
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (a < 24)
+	{
+		b = 0;
+		while (b < 60)
+		{
+		printf("%02d:%02d", a, b);
+		printf("\n");
+		b++;
+		}
+		a++;
+	}
+}	
