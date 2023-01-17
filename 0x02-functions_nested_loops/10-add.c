6deaed6b8fcda01baa633dd43ad33bfbf240539d
gedithecoder <gedionderejev@gmail.com>
2023-01-17 13:16:49 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * add - add two integers and return the result
+ * @a: integer to be added
+ * @b: integer to be added
+ *
+ * Return: addition
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
