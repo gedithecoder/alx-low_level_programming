b3955884c5909ba08e8d3e08ea63e11363fe560e
gedithecoder <gedionderejev@gmail.com>
2023-02-03 11:48:29 -0800

    Bit Manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks thendianness.
+ * 
+ * Return: If big-endin-0.
+ * If little-endin-1.
+ */
+
+int get_endianness(void)
+{
+	int num = 1;
+	char *endian = (char *)&num;
+
+	if (*endian == 1)
+		return (1);
+	return (0);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This directory contains a Bit manipulation project
