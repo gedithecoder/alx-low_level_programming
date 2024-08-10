3334266f901efe32aceab69132fc985538478cc2
gedithecoder <gedionderejev.com>
2024-08-10 04:45:52 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,7 +2,7 @@
 
 
    /**
-    * _print: moves a string one place to the left and prints the string
+    * _print - moves a string one place to the left and prints the string
     * @str: string to move
     * @l: size of string
     *
